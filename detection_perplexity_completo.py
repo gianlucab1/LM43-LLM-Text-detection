007cb3e2b61c20eabc9bbb7bfe085af6054e6288
Gianluca B <93779022+gianlucab1@users.noreply.github.com>
2025-09-09 11:35:41 +0200

    Update detection_perplexity_completo.py

--- detection_perplexity_completo.py
+++ detection_perplexity_completo.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import numpy as np
 import os
 import json
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_curve, auc
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import ttest_ind
